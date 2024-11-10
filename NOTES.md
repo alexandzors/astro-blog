b62d4e93a48b55cfa963cecd4dc1534d6792d73b
Stefan <47226125+StefanMarAntonsson@users.noreply.github.com>
2024-11-10 02:24:33 -0500

    added partytown

--- NOTES.md
+++ NOTES.md
@@ -7,8 +7,14 @@ Main page needs to be pretty
 Blog posts need to be updated to new formats
 
 Giscus
+
 Post nav on bottom shows 2 separate nav things
 
 Sidebar... ? the sidebar is a list of blog posts
     - what to do about that?
     - where to have the list of posts?
+Hard to see where one post ends and the next begins, visually in the blog list
+
+Look into Astro redirect
+
+Share post links for the blog posts, like share on fb, twitter, etc.
--- astro.config.ts
+++ astro.config.ts
@@ -4,31 +4,36 @@ import starlight from '@astrojs/starlight';
 import starlightBlog from 'starlight-blog';
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: "Alex's Blog",
-			plugins: [starlightBlog({
-				authors: {
-				  alex: {
-					name: 'Alex',
-					title: 'Wizard',
-					picture: '/profile_alex.jpg', // Images in the `public` directory are supported.
-					url: 'https://alexsguardian.net/',
-				  },
-				},
-			  })],
-			customCss: ['./src/blog-styles.css'],
-			expressiveCode: {
-				/* https://expressive-code.com/guides/themes/ */
-				themes: ['github-dark', 'github-light'],
-				// @ts-ignore
-				plugins: [pluginLineNumbers()],
-			},
-			social: {
-				github: 'https://github.com/alexandzors',
-			},
-		}),
-	],
+    integrations: [starlight({
+        title: "Alex's Blog",
+        plugins: [starlightBlog({
+            authors: {
+              alex: {
+                name: 'Alex',
+                title: 'Wizard',
+                picture: '/profile_alex.jpg', // Images in the `public` directory are supported.
+                url: 'https://alexsguardian.net/',
+              },
+            },
+          })],
+        customCss: ['./src/blog-styles.css'],
+        expressiveCode: {
+            /* https://expressive-code.com/guides/themes/ */
+            themes: ['github-dark', 'github-light'],
+            // @ts-ignore
+            plugins: [pluginLineNumbers()],
+        },
+        social: {
+            github: 'https://github.com/alexandzors',
+        },
+		}), partytown({
+			// https://docs.astro.build/en/guides/integrations-guide/partytown/
+
+			// Example: Disable debug mode.
+			config: { debug: false },
+		  })],
 });
