dea08933f79305f8c35a200b3e27e97ac3e1f322
Stefan <47226125+StefanMarAntonsson@users.noreply.github.com>
2024-11-13 14:54:32 -0500

    added components to config

--- astro.config.ts
+++ astro.config.ts
@@ -7,37 +7,47 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: "Alex's Blog",
-        plugins: [starlightBlog({
-          recentPostCount: 1,
-          prefix: 'posts',
-            authors: {
-              alex: {
-                name: 'Alexander Henderson',
-                title: 'Sometimes a "Developer"',
-                picture: '/profile_alex.jpg', // Images in the `public` directory are supported.
-                url: 'https://blog.alexsguardian.net/authors/alexander-henderson',
-              },
+  integrations: [starlight({
+    title: "Alex's Blog",
+      components: {
+        SiteTitle: './src/components/MyHeader.astro',
+        /* MarkdownContent: './src/components/MyHeader.astro', */
+      },
+      sidebar:[
+        {
+          label: 'Guides',
+          autogenerate: { directory: 'guides' },
+        },
+      ],
+      plugins: [starlightBlog({
+        recentPostCount: 1,
+        prefix: 'posts',
+          authors: {
+            alex: {
+              name: 'Alexander Henderson',
+              title: 'Sometimes a "Developer"',
+              picture: '/profile_alex.jpg', // Images in the `public` directory are supported.
+              url: 'https://blog.alexsguardian.net/authors/alexander-henderson',
             },
-          })],
-        customCss: ['./src/blog-styles.css'],
-        expressiveCode: {
-            /* https://expressive-code.com/guides/themes/ */
-            themes: ['github-dark', 'github-light'],
-            // @ts-ignore
-            plugins: [pluginLineNumbers()],
-        },
-        social: {
-			    blueSky: 'https://bsky.app/profile/alexandzors.bsky.social',
-			    youtube: 'https://www.youtube.com/@alexandzors',
-          github: 'https://github.com/alexandzors',
-			    email: 'mailto:blog@alexsguardian.net',
-        },
-		}), partytown({
-			// https://docs.astro.build/en/guides/integrations-guide/partytown/
-
-			// Example: Disable debug mode.
-			config: { debug: false },
-		  })],
+          },
+        })],
+    customCss: ['./src/blog-styles.css'],
+    expressiveCode: {
+        /* https://expressive-code.com/guides/themes/ */
+        themes: ['github-dark', 'github-light'],
+        // @ts-ignore
+        plugins: [pluginLineNumbers()],
+    },
+    social: {
+      blueSky: 'https://bsky.app/profile/alexandzors.bsky.social',
+      youtube: 'https://www.youtube.com/@alexandzors',
+      github: 'https://github.com/alexandzors',
+      email: 'mailto:blog@alexsguardian.net',
+    },
+  }),
+  partytown({
+    // https://docs.astro.build/en/guides/integrations-guide/partytown/
+    // Example: Disable debug mode.
+    config: { debug: false },
+  })],
 });
