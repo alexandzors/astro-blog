fa037c5401d16024fdb90e49df9394cbbe7d9f7a
Alexander Henderson <alexandzors@gmail.com>
2025-02-03 08:28:58 +0000

    fix footer logo

--- src/navigation.ts
+++ src/navigation.ts
@@ -38,7 +38,7 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="/src/assets/favicons/favicon.svg" alt="Alex's Guardian Logo" loading="lazy"></img>
     © 2025 - <a class="text-blue-600 underline dark:text-muted" href="https://github.com/alexandzors"> Alexander Henderson</a>. Some rights reserved. <span class="flex"> Powered by 🚀 <a class="text-blue-600 underline dark:text-muted" href="https://github.com/onwidget/astrowind"> Astrowind</a>.</span>
   `,
 };
